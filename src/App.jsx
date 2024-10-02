1feaecd2e3c56cb787d8440e9af212659ff35273
Pavan <pavanghuge2@gmail.com>
2024-10-03 01:07:19 +0530

    Basic Components

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import Header from "./Components/Header";
+import Sidebar from "./Components/Sidebar";
+import Footer from "./Components/Footer";
+import "./App.css";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import CreatePost from "./Components/CreatePost";
+import PostList from "./Components/PostList";
+import { useState } from "react";
+
+export default function App() {
+  const [selectedTab, setSelectedTab] = useState("CreatePost");
+  return (
+    <div className="app-conatiner">
+      <Sidebar selectedTab={selectedTab} setSelectedTab={setSelectedTab} />
+      <div className="content">
+        <Header />
+        {selectedTab === "Home" ? <PostList /> : <CreatePost />}
+
+        <Footer />
+      </div>
+    </div>
+  );
+}
---
+++ src/Components/PostList.jsx
@@ -0,0 +1,11 @@
+import Post from "./Post";
+
+export default function PostList() {
+  return (
+    <div>
+      <Post />
+      <Post />
+      <Post />
+    </div>
+  );
+}
